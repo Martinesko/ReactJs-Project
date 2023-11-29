d5a923704b6931e6c19dcfcc76f5d3bf76c8d3e6
Martinesko <87262511+Martinesko@users.noreply.github.com>
2023-11-29 18:29:26 +0200

    Added Header

---
+++ Client/src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+export default function Header(){
+    return(
+        <header>
+            <p1>Shop</p1>
+            <nav>
+               <div id="user">
+                   <a>Create</a>
+                   <a>My Listings</a>
+                   <a></a>
+                   <a>Logout</a>
+               </div>
+               <div id="guest">
+                   <a>Login</a>
+                   <a>Register</a>
+               </div>
+            </nav>
+        </header>
+    );
+}
