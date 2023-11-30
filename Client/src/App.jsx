580ca7b24f6d11bb14f16dedb9a8fb719d5ffaf5
Martinesko <87262511+Martinesko@users.noreply.github.com>
2023-11-30 18:41:37 +0200

    Simple Catalog view

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,13 @@
 import Header from "./components/header/Header.jsx"
 import Home from "./components/Home/Home.jsx"
+import Catalog from "./components/Catalog/Catalog.jsx"
 
 function App() {
   return (
     <div id="box">
         <Header />
 
-        <Home />
+        <Catalog/>
     </div>
   )
 }
