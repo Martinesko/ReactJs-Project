75846f557cdfd98c081daed517dc3620f76da632
Martinesko <87262511+Martinesko@users.noreply.github.com>
2023-12-08 15:35:00 +0200

    fix currency

--- Client/src/components/Catalog/Catalog-item/Catalog-item.jsx
+++ Client/src/components/Catalog/Catalog-item/Catalog-item.jsx
@@ -9,7 +9,7 @@ export default  function CatalogItem({_id,title,price,location,imageUrl}){
         <img className="catalog-image" src={image}/>
           <div className="information-Container">
             <h3 className="listing-title">{title}</h3>
-            <h3 className="price">{price} лв.</h3>
+            <h3 className="price">{price} lv.</h3>
             <p>{location} district</p>
             <div className="button-holder">
                 <button className="button" onClick={() => window.location.href=`http://127.0.0.1:5173/details/${_id}`}>Show</button>
